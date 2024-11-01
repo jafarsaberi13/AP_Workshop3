4fd6c0a9c7da452743d894f447d92ad3184fd9c2
Mohammad Jafar Jafar <jafarkashify@gmail.com>
2024-11-01 21:34:36 +0330

    PhoneNumber class created

---
+++ PhoneNumber.java
@@ -0,0 +1,14 @@
+public class PhoneNumber {
+    private String countryCode;
+    private String number;
+
+    public PhoneNumber(String countryCode, String number) {
+        this.countryCode = countryCode;
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Country Code: " + countryCode + ", Number: " + number;
+    }
+}
